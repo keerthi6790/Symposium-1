d1bef0472fcb9916502d351d44cea182bbc311bb
M-Dinesh-104 <dineshmurugesan107@gmail.com>
2022-05-07 19:08:17 +0530

    first commit

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -1,19 +1,107 @@
-import React from "react";
-import {TechnicalDetails,NonTechnicalDetails,WorkshopDetails} from '../../Details'
-export default function Details({props}){
-    let component;
-    function DetailsRender(){
-        var a=window.location.pathname.split('/');
-        if(a[2]==='Technical'){
-            component=TechnicalDetails.find((e)=>{
-                e.id===a[3]
+import { Button, Tab, Tabs, Typography } from "@mui/material";
+import PropTypes from 'prop-types';
+import { Box } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { TechnicalDetails, NonTechnicalDetails, WorkshopDetails } from '../../Details';
+import NavBar from '../NavBar/NavBar'
+import './Details.css'
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+export default function Details() {
+    const [Component, setComponent] = useState({})
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
+    useEffect(() => {
+        DetailsRender()
+    },[])
+    function DetailsRender() {
+        var a = window.location.pathname.split('/');
+        let x;
+        if (a[2] === 'Technical') {
+            x=TechnicalDetails.find((e) => {
+                return (
+                    e.id === a[3]
+                )
             })
         }
-        console.log(component)
+        setComponent(x);   
+    console.log(Component) 
+
     }
-    return(
-        <div>
-            {DetailsRender()}
+    return (
+        <div className="Details">
+            <NavBar color="white"/>
+        <div className="DetailsFlex">
+            <h1 className="DetailsTitle">
+                {Component.name}
+            </h1>
+            <div style={{display:"flex",flexDirection:"row",gap:"30px"}}>
+             <button className="DetailsButton"  onClick={()=>{window.location.href='#Goto'}}>Know More</button>
+             <button className="DetailsButton">Register</button>
+            </div>
+        </div>
+        <div id="Goto">
+        <Box sx={{ borderRadius:1,backgroundColor:"white",m:10 }}>
+  <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+    <Tab label="Description" {...a11yProps(0)} />
+    <Tab label="Rules and Regulations" {...a11yProps(1)} />
+    <Tab label="Required" {...a11yProps(2)} />
+  </Tabs>
+<TabPanel value={value} index={0}>
+  {Component.description}
+</TabPanel>
+<TabPanel value={value} index={1}>
+  {Component.rules.map(r=>{
+      console.log(r)
+      return(
+          <ul>
+              <li>{r.Zero}</li>
+          </ul>
+      )
+  })}
+  
+</TabPanel>
+<TabPanel value={value} index={2}>
+{Component.required}
+</TabPanel>
+</Box>
+
+        </div>
         </div>
     )
 }
--- src/Details.js
+++ src/Details.js
@@ -3,95 +3,109 @@ import TechnicalImg from "./Technical.png";
 import NonTechnical from "./NonTechnical.png";
 import Workshop from "./Workshop.png";
 export const TechnicalDetails = [
-    {
-      id:"Dinesh",
-      name: "Dinesh",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${TechnicalImg}`,
-      Link:"Dinesh"
-    },
-    {
-      id:"Webdevelopment",
-      name: "web development",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${TechnicalImg}`,
-      Link:"Webdevelopment"
-    },
-    {
-      id:"Appdevelopment",
-      name: "App development",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${NonTechnical}`,
-      Link:"Appdevelopment"
-    },
-    {
-      id:"TreasureHunt",
-      name: "Treasure Hunt",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${Workshop}`,
-      Link:"TreasureHunt"
-    },
-  ];
+  {
+    id: "Dinesh",
+    name: "Dinesh",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${TechnicalImg}`,
+    Link: "Dinesh",
+    rules: [{
+      Zero: "hasjsadj jsahdd sjdh asd hshandhg ashbdn bhsd nbbjh  dshadbnbhbasnd "
+    }, {
+      First: "sakdn sahdj jsa h asdkj jkasn dbhj dasb nsjam js "
+    }
+    ],
+    required: "sja sad sahdj shd jkks aj sjd hjsa jsad kjksnm"
+  },
+  {
+    id: "Webdevelopment",
+    name: "Web development",
+    description:
+      " sajdjkasd It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${TechnicalImg}`,
+    Link: "Webdevelopment",
+    rules: "lorem amsdjasdjajdj asnkjdaks jahsdjha sakjdha sandjaj knjashjdh nnkjdhj nnsadkjhkja asjkdhkja khkjsa jhsjakd jnjasd jn dnkjk nnsajnd nnkj nkj asdn jkn",
+    required: "sja sad sahdj shd jkks aj sjd hjsa jsad kjksnm"
+
+  },
+  {
+    id: "Appdevelopment",
+    name: "App development",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${NonTechnical}`,
+    Link: "Appdevelopment",
+    rules: "lorem amsdjasdjajdj asnkjdaks jahsdjha sakjdha sandjaj knjashjdh nnkjdhj nnsadkjhkja asjkdhkja khkjsa jhsjakd jnjasd jn dnkjk nnsajnd nnkj nkj asdn jkn",
+    required: "sja sad sahdj shd jkks aj sjd hjsa jsad kjksnm"
+
+  },
+  {
+    id: "TreasureHunt",
+    name: "Treasure Hunt",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${Workshop}`,
+    Link: "TreasureHunt",
+    rules: "lorem amsdjasdjajdj asnkjdaks jahsdjha sakjdha sandjaj knjashjdh nnkjdhj nnsadkjhkja asjkdhkja khkjsa jhsjakd jnjasd jn dnkjk nnsajnd nnkj nkj asdn jkn",
+    required: "sajkdjashdjadjhsaj sahdj sadj sjdhja sjda"
+  },
+];
 export const NonTechnicalDetails = [
-    {
-      name: "Paper presentation",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${TechnicalImg}`,
-    },
-    {
-      name: "App development",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${NonTechnical}`,
-    },
-    {
-      name: "Treasure Hunt",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${Workshop}`,
-    },
-    {
-      name: "Treasure Hunt",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${Workshop}`,
-    },
-  ];
+  {
+    name: "Paper presentation",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${TechnicalImg}`,
+  },
+  {
+    name: "App development",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${NonTechnical}`,
+  },
+  {
+    name: "Treasure Hunt",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${Workshop}`,
+  },
+  {
+    name: "Treasure Hunt",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${Workshop}`,
+  },
+];
 export const WorkshopDetails = [
-    {
-      name: "Paper presentation",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${TechnicalImg}`,
-    },
-    {
-      name: "App development",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${NonTechnical}`,
-    },
-    {
-      name: "Treasure Hunt",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${Workshop}`,
-    },
-    {
-      name: "Treasure Hunt",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${Workshop}`,
-    },
-    {
-      name: "Treasure Hunt",
-      description:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
-      img: `${Workshop}`,
-    },
-  ];
-  
+  {
+    name: "Paper presentation",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${TechnicalImg}`,
+  },
+  {
+    name: "App development",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${NonTechnical}`,
+  },
+  {
+    name: "Treasure Hunt",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${Workshop}`,
+  },
+  {
+    name: "Treasure Hunt",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${Workshop}`,
+  },
+  {
+    name: "Treasure Hunt",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors",
+    img: `${Workshop}`,
+  },
+];
